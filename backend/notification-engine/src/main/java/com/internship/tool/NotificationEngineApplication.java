3ba0ac11c2f2021d6e54edba1d3a22e6513d9a57
Sukumar RA <sukumaramaravathi@gmail.com>
2026-04-27 17:52:29 +0530

    Day 1 — Setup Spring Boot project structure and application.yml

---
+++ backend/notification-engine/src/main/java/com/internship/tool/NotificationEngineApplication.java
@@ -0,0 +1,12 @@
+package com.internship.tool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationEngineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationEngineApplication.class, args);
+    }
+}
